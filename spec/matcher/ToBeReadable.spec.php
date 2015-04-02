df92d08215afb577e60d700c936da2e53634cdef
holyshared <holy.shared.design@gmail.com>
2015-04-02 11:06:15 +0900

    Add to be readable matcher failed message

--- spec/matcher/ToBeReadable.spec.php
+++ spec/matcher/ToBeReadable.spec.php
@@ -28,4 +28,30 @@ describe('ToBeReadable', function() {
             });
         });
     });
+
+    describe('#reportFailed', function () {
+        beforeEach(function () {
+            $this->tempFile = $this->makeFile(0444);
+            $this->matcher = new ToBeReadable();
+            $this->message = new FailedMessage();
+        });
+        it('report failed message', function() {
+            $this->matcher->match($this->tempFile->getPath());
+            $this->matcher->reportFailed($this->message);
+            Assertion::same((string) $this->message, "Expected '{$this->tempFile->getPath()}' to be readable");
+        });
+    });
+    describe('#reportNegativeFailed', function () {
+        beforeEach(function () {
+            $this->tempFile = $this->makeFile(0222);
+            $this->matcher = new ToBeReadable();
+            $this->message = new FailedMessage();
+        });
+        it('report failed message', function() {
+            $this->matcher->match($this->tempFile->getPath());
+            $this->matcher->reportNegativeFailed($this->message);
+            Assertion::same((string) $this->message, "Expected '{$this->tempFile->getPath()}' not to be readable");
+        });
+    });
+
 });
